e58d9fd429226700c3955c8c2f5f30a145853046
cmaks <cmaks@umich.edu>
2019-08-05 16:10:23 -0400

    28264ce2381d6f4d7eaf13b49d2fdb79

--- self_commit.py
+++ self_commit.py
@@ -1,8 +1,17 @@
 import os
 import time
+import hashlib
+import datetime
+
+
 def commit():
     os.system("git add .")
+    print("Added")
     time.sleep(1)
-    os.system("git commit -m \"Test Commit\"")
+    hash_object = hashlib.md5(str(datetime.datetime.now()).encode())
+    msg = "git commit -m \"" + hash_object.hexdigest() + "\""
+    os.system(msg)
+    print("Committed")
     time.sleep(1)
     os.system("git push")
+    print("Pushed")
